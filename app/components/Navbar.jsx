f876a295fb2b3e5da71566f062845bff3570623d
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2024-02-23 22:13:49 +0200

    Profile Dark

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -114,14 +114,14 @@ export default function Example() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white   py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-[#1e1e1e]  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
                             href="/profile"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "bg-gray-100 dark:bg-[#66bfbf]  " : "",
+                              "block px-4 py-2 text-sm text-gray-700 dark:text-gray-300"
                             )}
                           >
                             Your Profile
@@ -133,8 +133,8 @@ export default function Example() {
                           <a
                             href="#"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "bg-gray-100 dark:bg-[#66bfbf] " : "",
+                              "block px-4 py-2 text-sm text-gray-700 dark:text-gray-300"
                             )}
                           >
                             Settings
@@ -146,8 +146,8 @@ export default function Example() {
                           <a
                             href="#"
                             className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
+                              active ? "bg-gray-100 dark:bg-[#66bfbf] " : "",
+                              "block px-4 py-2 text-sm text-gray-700 dark:text-gray-300"
                             )}
                           >
                             Sign out
