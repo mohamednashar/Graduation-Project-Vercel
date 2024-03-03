b7b33321617349e69cb007d2953fd3aab8a4a15a
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2024-03-03 22:24:32 +0200

    Forms

--- app/mainStaff/page.jsx
+++ app/mainStaff/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { Button, Dialog, DialogBody } from "@material-tailwind/react";
 import { useState } from "react";
 
 const labels = [
@@ -19,22 +20,14 @@ const labels = [
 ];
 
 const Page = () => {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    secondName: "",
-    thirdName: "",
-    fourthName: "",
-    address: "",
-    username: "",
-    mail: "",
-    password: "",
-    dateOfBirth: "",
-    phone: "",
-    college: "",
-    department: "",
-    gpa: "",
-    gender: "",
-  });
+  const [formData, setFormData] = useState({});
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
+   
+  
 
   const [isMale, setIsMale] = useState(true);
   const [isFemale, setIsFemale] = useState(false);
@@ -76,19 +69,58 @@ const Page = () => {
         {label}
       </label>
       {type === "select" ? (
-        <select
-          name={name}
-          id={id}
-          value={formData[id]}
-          onChange={handleInputChange}
-          className="w-full block p-2 text-gray-900 border border-gray-300 rounded-lg bg-white sm:text-xs dark:bg-[#3f3f3f] dark:text-white"
-        >
-          {[1, 2, 3, 4].map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
+      <select
+      id={id}
+      value={formData[id]}
+      onChange={handleInputChange}
+      className="w-full block p-2 text-gray-900 border border-gray-300 rounded-lg bg-white sm:text-xs dark:bg-[#3f3f3f] dark:text-white"
+    >
+      {id === "college" ? (
+        // Options for the "College" select
+        [ "College A", "College B", "College C", "College D"].map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))
+      ) : id === "department" ? (
+        // Options for the "Department" select
+        ["Department X", "Department Y", "Department Z"].map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))
+      ) : (
+        // Default options (you can customize this as needed)
+        [1, 2, 3, 4].map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))
+      )}
+    </select>
+      ) : type === "radio" ? (
+        <div className="flex items-center">
+          <input
+            type="radio"
+            id={id + "Male"}
+            name={id}
+            checked={isMale && name === "checkboxMale"}
+            onChange={handleCheckboxChange}
+            className="mr-2"
+          />
+          <label htmlFor={id + "Male"} className="mr-5">
+            Male
+          </label>
+          <input
+            className="mr-2"
+            type="radio"
+            id={id + "Female"}
+            name={id}
+            checked={isFemale && name === "checkboxFemale"}
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor={id + "Female"}>Female</label>
+        </div>
       ) : (
         <input
           type={type}
@@ -103,7 +135,7 @@ const Page = () => {
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col">
-      <div className="bg-white mx-5 md:mx-20 p-5 rounded-lg shadow-md flex flex-wrap justify-center lg:justify-between">
+      <div className="bg-white mx-5 md:mx-20 lg:mx-36 p-5 rounded-lg shadow-md flex flex-wrap justify-center lg:justify-between">
         {labels.map((label, index) => (
           <div key={index} className="">
             {index === labels.length - 1 ? (
@@ -137,19 +169,80 @@ const Page = () => {
             ) : (
               renderInput(
                 label,
-                index === 8 ? "date" : index === 13 ? "radio" : "text",
+                index === 8 ? "date" : index === 13 ? "radio" : index === 10 || index === 11 ? "select" : "text",
                 label.toLowerCase()
               )
             )}
           </div>
         ))}
-         
-       
       </div>
-      <button type="submit" className="bg-[#66bfbf] text-white px-4 py-2 mt-4 rounded-lg w-[30%] mx-auto mb-5">
-          Submit
-        </button>
-      
+      <Button
+        onClick={handleOpen}
+        type="submit"
+        className="font-bold text-lg bg-[#66bfbf] text-white px-4 py-2 mt-4 rounded-lg w-[30%] mx-auto mb-5 transition-all duration-200 hover:bg-[#f76b8a]"
+        data-dialog-target="animated-dialog"
+      >
+        Submit
+      </Button>
+
+      <Dialog
+        className="!w-96"
+        open={open}
+        handler={handleOpen}
+        animate={{
+          mount: { scale: 1, y: 0 },
+          unmount: { scale: 0.9, y: -100 },
+        }}
+      >
+        <DialogBody>
+          {/* Modal content */}
+          <div className=" text-center bg-white rounded-lg dark:bg-gray-800 p-5">
+            <button
+              onClick={handleOpen}
+              className="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+            <div className="w-12 h-12 rounded-full bg-green-100 dark:bg-green-900 p-2 flex items-center justify-center mx-auto mb-3.5">
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 text-green-500 dark:text-green-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="sr-only">Success</span>
+            </div>
+            <p className="mb-4 text-lg font-semibold text-gray-900 dark:text-white">
+              Submitted Successfully
+            </p>
+            <button
+              onClick={handleOpen}
+              class="middle none center rounded-lg bg-gradient-to-tr from-green-600 to-green-400 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            >
+              Continue
+            </button>
+          </div>
+        </DialogBody>
+      </Dialog>
     </form>
   );
 };
