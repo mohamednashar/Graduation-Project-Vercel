2a794d2bd9076bb92b6ce5b0151fea6a7e636e03
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2024-03-09 18:06:02 +0200

    Create Quiz (Not Finished)

--- app/quizzes/page.jsx
+++ app/quizzes/page.jsx
@@ -1,9 +1,11 @@
+import CreateQuiz from "./CreateQuiz";
 import QuizList from "./QuizList";
 
 const page = () => {
   return (
     <div className="mt-5">
-            <QuizList />
+            {/* <QuizList /> */}
+            <CreateQuiz />
     </div>
   );
 };
