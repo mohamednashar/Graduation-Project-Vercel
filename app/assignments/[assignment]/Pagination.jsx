007b27cc60a3e6ae730b8987bca055881ceb5f2b
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2024-03-16 02:06:39 +0200

    add style pagination

--- app/assignments/[assignment]/Pagination.jsx
+++ app/assignments/[assignment]/Pagination.jsx
@@ -26,11 +26,11 @@ export default function Pagination() {
         onClick={prev}
         disabled={active === 1}
       >
-        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
+        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4 dark:text-[#66bfbf]" />
       </IconButton>
-      <Typography color="gray" className="font-normal">
-        Page <strong className="text-gray-900">{active}</strong> of{" "}
-        <strong className="text-gray-900">10</strong>
+      <Typography  className="font-normal dark:text-[#66bfbf]">
+        Page <strong className="text-gray-900 dark:text-[#66bfbf]">{active}</strong> of{" "}
+        <strong className="text-gray-900 dark:text-[#66bfbf]">10</strong>
       </Typography>
       <IconButton
         size="sm"
@@ -38,7 +38,7 @@ export default function Pagination() {
         onClick={next}
         disabled={active === 10}
       >
-        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+        <ArrowRightIcon strokeWidth={2} className="h-4 w-4 dark:text-[#66bfbf]" />
       </IconButton>
     </div>
   );
