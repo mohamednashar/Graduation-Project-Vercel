0f8807211e9dc4da5b48eb876df6739edc383b20
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2024-03-19 02:09:00 +0200

    add assignment style

--- app/assignments/AssignmentProf.jsx
+++ app/assignments/AssignmentProf.jsx
@@ -6,9 +6,9 @@ function AssignmentProf() {
 
   return (
     <div className="">
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 p-4 gap-5">
+      <div className="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 p-4 gap-5 ">
       
-          <AssignmentCardProf  subject={"DatabAse"} college={"Engineering"} department ={"Control"}  group={"1"} code={"data-13"}/>
+          <AssignmentCardProf  subject={"Database"} college={"Engineering"} department ={"Control"}  group={"1"} code={"data-13"}/>
       
           <AssignmentCardProf  subject={"Algorithms"} college={"Computer Science"} department ={"IT"}  group={"2"} code={"Algo-14"} />
           <AssignmentCardProf  subject={"C++"} college={"Engineering"} department ={"Computer Science"}  group={"2"} code={"Cpp-25"} />
--- app/components/AssignmentCardProf.jsx
+++ app/components/AssignmentCardProf.jsx
@@ -4,26 +4,26 @@ import React from 'react';
 const AssignmentCardProf = ({ subject, college, department ,  group, code }) => {
   return (
     <div className="bg-white dark:bg-[#1e1e1e] rounded-lg shadow-lg p-6 mb-6 w-full max-w-3xl mx-auto transition-transform duration-300 transform hover:scale-105">
-      <div className="text-2xl font-bold mb-2 text-[#66bfbf]">{subject}</div>
+      <div className="text-2xl font-bold mb-2 text-[#66bfbf] ">{subject}</div>
       <div className="grid grid-cols-2 gap-4 mb-4">
         <div className="text-gray-600 dark:text-gray-100">
-          <span className="font-bold">college:</span> {college}
+          <span className="font-bold lg:line-clamp-1  ">college: {college}</span> 
         </div>
         <div className="text-gray-600 dark:text-gray-100">
-            <span className="font-bold">department: {department}</span> 
+            <span className="font-bold lg:line-clamp-1  ">department: {department}</span> 
         </div>
       </div>
       <div className="grid grid-cols-2 gap-4 mb-4">
         <div className="text-gray-700 dark:text-gray-200">
-          <span className="font-bold">Group:</span> {group}
+          <span className="font-bold lg:line-clamp-1  ">Group:  {group}</span>
         </div>
-        <div className="text-gray-700 dark:text-gray-200">
-          <span className="font-bold">Course code</span> {code}
+        <div className="text-gray-700 dark:text-gray-200 ">
+          <span className="font-bold lg:line-clamp-1  ">Course code {code}</span> 
         </div>
       </div>
     
 
-      <Link href={`/assignments/${code}-${group}`}>
+      <Link href={`/assignments/${code}-${group}`} >
       <button  className="bg-[#66bfbf] text-white px-4 py-2 rounded-md font-semibold hover:bg-[#529d9d] transition-all duration-200 w-full">
         Assignment details
       </button>
