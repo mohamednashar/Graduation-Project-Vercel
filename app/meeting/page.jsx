7401f5d6870800c09dc8878f208a0e897fa5167b
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2024-04-05 06:09:25 +0200

    z

--- app/meeting/page.jsx
+++ app/meeting/page.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 async function getData() {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/12')
-
   if(!res.ok)
   {
     throw new Error("Failed")
   }
  
   return res.json()
+
 }
 
 export default async function page() {
   const data = await getData()
+  console.log(data)
   return (
     <div>
       {data.title}
