c371d8c427b21f2eb5bff5dcf502054e79034255
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2024-04-15 23:12:03 +0200

    setup end points

--- app/meeting/page.jsx
+++ app/meeting/page.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import axios from 'axios';
 
-async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos/12')
-  if(!res.ok)
-  {
-    throw new Error("Failed")
+
+const API = process.env.NEXT_PUBLIC_BACKEND_API ;
+
+export const postAcademicYear = async () => {
+  try {
+    const response = await axios.get(`${API}Faculty/GetFaculties`);
+    console.log(response.data); // Log the response data if needed
+    return response.data; // Return the response data if needed
+  } catch (error) {
+    console.error("Error:", error);
+    throw error; // Throw the error to handle it wherever  this function is called
   }
- 
-  return res.json()
+};
 
-}
-
-export default async function page() {
-  const data = await getData()
-  console.log(data)
-  return (
-    <div>
-      {data.title}
-      
-    </div>
-  )
+export default async function Page() {
+  try {
+    await postAcademicYear(); // Call the function to send POST request
+    return (
+      <div>
+        adlfkjalkg
+      </div>
+    );
+  } catch (error) {
+    // Handle error if needed
+    console.error("Error:", error);
+    return (
+      <div>
+        Error occurred: {error.message}
+      </div>
+    );
+  }
 }
