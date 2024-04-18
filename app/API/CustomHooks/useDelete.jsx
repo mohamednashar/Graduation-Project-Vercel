ef50e479bb194bc4a8dd7fa904bc3292556f61a1
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2024-04-18 21:54:15 +0200

    make delete request function

--- app/API/CustomHooks/useDelete.jsx
+++ app/API/CustomHooks/useDelete.jsx
@@ -1,25 +1,19 @@
-export const  deleteData = async(link , id)=> {
+import axios from 'axios';
+
+// Function to make a DELETE request
+export const deleteData = async (url, headers, bodyData) => {
   try {
-    const API =process.env.NEXT_PUBLIC_BACKEND_API
+    const API = process.env.NEXT_PUBLIC_BACKEND_API;
 
-    const response = await fetch(`${API}${link}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        // You can add more headers if needed
-      },
-      // You can include a body if required for your API
-       body: JSON.stringify({ id: id }),
+    const response = await axios.delete(`${API}${url}`, {
+      headers: headers,
+      data: bodyData
     });
-    
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
 
-    // If the response is successful, you can handle it here
-    console.log('Data deleted successfully');
+    console.log('Delete request successful', response.data);
+    // Handle response data or state update as needed
   } catch (error) {
-    // Handle errors here
-    console.error('There was a problem with the delete request:', error.message);
+    console.error('Error making delete request', error);
+    // Handle error as needed
   }
-}
+};
