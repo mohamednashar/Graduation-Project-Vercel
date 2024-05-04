a55cbd7c54b1e8a34a65d30e3812da8478e89415
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2024-05-04 19:33:55 +0300

    Final Push For Login Stuff + Create Quiz

--- app/quizzes/page.jsx
+++ app/quizzes/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import CreateQuiz from './CreateQuiz'
 
@@ -6,23 +5,8 @@ const page = () => {
   return (
     <div>
       <CreateQuiz/>
-=======
-import CreateQuiz from "./CreateQuiz";
-import QuizList from "./QuizList";
-
-const page = () => {
-  return (
-    <div className="mt-5">
-            {/* <QuizList /> */}
-            <CreateQuiz />
->>>>>>> 0646e15b9a92e84451977baa59b44915c81257be
     </div>
   )
 }
 
-<<<<<<< HEAD
 export default page
-=======
-export default page;
-
->>>>>>> 0646e15b9a92e84451977baa59b44915c81257be
