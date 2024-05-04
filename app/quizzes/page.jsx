0c157a8665126b79d05a31d2bd21bd808ad005a5
mohamednashar <105996910+mohamednashar@users.noreply.github.com>
2024-05-04 19:31:04 +0300

    z

---
+++ app/quizzes/page.jsx
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+import React from 'react'
+import CreateQuiz from './CreateQuiz'
+
+const page = () => {
+  return (
+    <div>
+      <CreateQuiz/>
+=======
+import CreateQuiz from "./CreateQuiz";
+import QuizList from "./QuizList";
+
+const page = () => {
+  return (
+    <div className="mt-5">
+            {/* <QuizList /> */}
+            <CreateQuiz />
+>>>>>>> 0646e15b9a92e84451977baa59b44915c81257be
+    </div>
+  )
+}
+
+<<<<<<< HEAD
+export default page
+=======
+export default page;
+
+>>>>>>> 0646e15b9a92e84451977baa59b44915c81257be
